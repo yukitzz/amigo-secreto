c2c51a579ff56fed2f1548773b384382f4778eac
yukitzz <thajlalawanesousalopes@gmail.com>
2025-02-17 10:52:03 -0300

    amigo secreto

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-Array.prototype.remove = function() {
-document.getElementById('')
-}
+let meuArray = []
