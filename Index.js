20cbaa2cfdd5c5c5af820d63c298889b2474743d
yukitzz <thajlalawanesousalopes@gmail.com>
2025-02-24 15:01:43 -0300

    atualizado

--- Index.js
+++ Index.js
@@ -1,15 +1,59 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let array = [];
-function adicionarAmigo(){
-    array= document.getElementById("amigo");
-    let nome= amigo.value;
-    document.getElementById("listaAmigos");
-    let listaAmigos= document.getElementById("listaAmigos");
-    let novoNome= document.createElement("li");
+//array para armazenar os nomes 
+let amigos = [];
+function adicionarAmigo() {
+    let inputElement = document.getElementById('amigo');
+    let nome = inputElement.value.trim();
 
-    novoNome.textContent=nome;
+    if (!nome) {
+        alert('Por favor, insira um nome');
+        return;
+    }
 
-    listaAmigos.appendChild(novoNome);
+    if (amigos.includes(nome)) {
+        alert('O nome já existe, por favor digite outro');
+        return;
+    }
 
-    array.value="";
+    amigos.push(nome);
+    console.log(amigos);
+    atualizarAmigos();
+    limparCampo();
+}
+
+
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
+
+// atualizar lista de amigos 
+function atualizarAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = ''
+
+    for (let i = 0; i < amigos.length; i++) {
+        const li = document.createElement('li')
+        li.textContent = amigos[i]
+        lista.appendChild(li)
+    }
+}
+
+// sortear amigos 
+function sortearAmigo() {
+    if (amigos == [] || amigos.length == 0) {
+        alert('Por favor, adicione um nome antes de sortear')
+    } else {
+        const index = Math.floor(Math.random() * amigos.length)
+        const amigoSecreto = amigos[index]
+
+        const ul = document.getElementById('resultado')
+        ul.innerHTML = ''
+
+        const li = document.createElement('li')
+        li.textContent = `Seu amigo secreto sorteado é: ${amigoSecreto}`
+        ul.appendChild(li)
+        dispararConfete();
+        amigos = [];
+        atualizarAmigos();
+    }
 }
